439e537eb4b20cc2848ae25a5d4767a0c02396d0
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-04-27 23:10:29 +0800

    add source code to build rviz2 panel

---
+++ include/rviz2-panel/rviz2_panel.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+// ROS2
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
+// RVIZ2
+#include <rviz_common/panel.hpp>
+// Qt
+#include <QtWidgets>
+/** 
+ *  Include header generated from ui file
+ *  Note that you will need to use add_library function first
+ *  in order to generate the header file from ui.
+ */
+#include <ui_push_button.h>
+
+namespace custom_panel
+{
+  class RvizPushButtonPanel : public rviz_common::Panel
+  {
+    Q_OBJECT
+  public:
+    explicit RvizPushButtonPanel(QWidget *parent = nullptr);
+    ~RvizPushButtonPanel();
+
+    /// Load and save configuration data
+    virtual void load(const rviz_common::Config &config) override;
+    virtual void save(rviz_common::Config config) const override;
+
+  private Q_SLOTS:
+    void on_pushButton1_clicked();
+    void on_pushButton2_clicked();
+
+  private:
+    std::unique_ptr<Ui::gui> ui_;
+    rclcpp::Node::SharedPtr node_;
+    uint16_t count_button_1_, count_button_2_;
+
+  protected:
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button1_pub_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button2_pub_;
+    std_msgs::msg::Bool msg_;
+  };
+} // custom_panel
---
+++ src/rviz2_panel.cpp
@@ -0,0 +1,73 @@
+#include "rviz2-panel/rviz2_panel.hpp"
+#include <memory>
+#include <rviz_common/config.hpp>
+
+namespace custom_panel
+{
+  RvizPushButtonPanel::RvizPushButtonPanel(QWidget *parent)
+    : Panel{parent}
+    , ui_(std::make_unique<Ui::gui>())
+    , node_{nullptr}
+    , count_button_1_{0}
+    , count_button_2_{0}
+  {
+    // Extend the widget with all attributes and children from UI file
+    ui_->setupUi(this);
+
+    // Init rclcpp node
+    auto options = rclcpp::NodeOptions().arguments(
+        {"--ros-args", "--remap", "__node:=rviz_push_button_node", "--"});
+    node_ = std::make_shared<rclcpp::Node>("_", options);
+
+    button1_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button1", 1);
+    button2_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button2", 1);
+
+    // Prepare msg
+    msg_.data = true;
+  }
+
+  RvizPushButtonPanel::~RvizPushButtonPanel()
+  {
+  }
+    void RvizPushButtonPanel::load(const rviz_common::Config &config)
+    {
+      Panel::load(config);
+      if (auto push_button_config = config.mapGetChild({"PushButton"}); push_button_config.isValid())
+      {
+        if (QVariant count_button_1 {0}; push_button_config.mapGetValue({"count_button_1"}, &count_button_1))
+        {
+          RCLCPP_INFO_STREAM(node_->get_logger(), "Button 1 was pressed " << count_button_1.toDouble() << " the last time.");
+        }
+        if (QVariant count_button_2 {0}; push_button_config.mapGetValue({"count_button_2"}, &count_button_2))
+        {
+          RCLCPP_INFO_STREAM(node_->get_logger(), "Button 2 was pressed " << count_button_2.toDouble() << " the last time.");
+        }
+      }
+    }
+
+    void RvizPushButtonPanel::save(rviz_common::Config config) const
+    {
+      Panel::save(config);
+      rviz_common::Config push_button_config = config.mapMakeChild({"PushButton"});
+      push_button_config.mapSetValue({"count_button_1"}, {count_button_1_});
+      push_button_config.mapSetValue({"count_button_2"}, {count_button_2_});
+    }
+
+    void RvizPushButtonPanel::on_pushButton1_clicked()
+    {
+      RCLCPP_INFO_STREAM(node_->get_logger(), "Published to button1 topic!");
+      button1_pub_->publish(msg_);
+      ui_->label_1->setText(QString::fromStdString({std::string{"Button 1 was clicked "} + std::to_string(++count_button_1_)}));
+    }
+
+    void RvizPushButtonPanel::on_pushButton2_clicked()
+    {
+      RCLCPP_INFO_STREAM(node_->get_logger(), "Published to button2 topic!");
+      button2_pub_->publish(msg_);
+      ui_->label_2->setText(QString::fromStdString({std::string{"Button 2 was clicked "} + std::to_string(++count_button_2_)}));
+    }
+
+} // custom_panel
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(custom_panel::RvizPushButtonPanel, rviz_common::Panel)
